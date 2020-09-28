566f3464b075856a2c47adcf794f8014a1f960a6
pg <paavo.gaidach@gmail.com>
2020-09-28 15:29:35 +0300

    5+6

--- 1-5/index.php
+++ 1-5/index.php
@@ -1,1 +1,12 @@
 <?php
+
+foreach (range('A', 'K') as $char) {
+    echo $char," "; 
+}
+
+echo "<br>";
+
+ foreach (range('K', 'A') as $char) {
+    echo $char, " ";
+	
+}
--- 1-6/index.php
+++ 1-6/index.php
@@ -1,1 +1,7 @@
 <?php
+
+$aakkoset = range('A','Z');
+
+$numero = array_search('M', $aakkoset);
+
+echo $numero + 1;
