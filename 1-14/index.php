98fa250c6a737cde3abb4c6a9ecdca1045183826
pg <paavo.gaidach@gmail.com>
2020-09-29 12:35:53 +0300

    14 ja 15

--- 1-14/index.php
+++ 1-14/index.php
@@ -1,3 +1,7 @@
 <?php
 
 $array = ['Hello', 'World', 'This', 'Is', 'Foreach', 'Loop', 'PHP is easy and fun!'];
+
+foreach ($array as $i => $arvo){
+    echo $arvo , "<br>";
+}
--- 1-15/index.php
+++ 1-15/index.php
@@ -1,1 +1,10 @@
 <?php
+
+$start = 0.0;
+$end = 10.5;
+$step = 0.5;
+
+for ($i = $start; $i < $end; $i+=$step)
+{
+    echo $i , ", ";
+}
