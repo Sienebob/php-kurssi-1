e89607d1a2e673a790f64bc088e96b6e0928f966
pg <paavo.gaidach@gmail.com>
2020-10-06 13:56:12 +0300

    20

--- 1-20/index.php
+++ 1-20/index.php
@@ -1,1 +1,30 @@
 <?php
+$rows = file(__DIR__ . '/maakunnat.txt');
+
+//print_r($rows);
+//var_dump($rows); 
+
+
+
+foreach ($rows as $row)
+    {
+        $parts = explode (';' , $row);
+
+        $list [$parts[0] ] = intval($parts[1]);
+
+    }
+
+arsort($list);
+
+$text = "";
+foreach($list as $maakunta => $vakiluku)
+    {
+        if ($vakiluku %2 !== 0)
+            {
+                $text .= "$maakunta;$vakiluku\n";
+            }
+    }
+
+file_put_contents(__DIR__ . '/maakunta_vastaus.txt', $text);
+
+//var_dump ($text);
